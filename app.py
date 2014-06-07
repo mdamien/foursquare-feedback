d9d6f0c00769207bb5bdabc3e8dceeb56be63239
Damien MARIE <damien@dam.io>
2014-06-07 17:04:12 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, render_template, redirect
+import requests
+import fsq
+app = Flask(__name__)
+
+@app.route("/post", methods=['POST',])
+def post():
+    #post message to page
+    venueId = request.form['venueId'] 
+    text = request.form['text']
+    payload = {'venueId': venueId, 'text': text }
+    response = requests.post('https://api.foursquare.com/v2/tips/add?oauth_token=0Q235XWN4ACGAPLRUTRUGBQFV1HWN4RVXCMHVE0EAETX2DMC&v=20140607', data=payload) 
+    print(response.text)
+    url = response.json()['response']['tip']['canonicalUrl']
+    return redirect(url+'/')
+
+@app.route("/")
+def index():
+    stores = fsq.search(40.7, -74)
+    return render_template('index.html', stores=stores)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ fsq.py
@@ -0,0 +1,18 @@
+import requests
+
+SUFFIX = '?oauth_token=0Q235XWN4ACGAPLRUTRUGBQFV1HWN4RVXCMHVE0EAETX2DMC&v=20140607'
+BASE = 'https://api.foursquare.com/v2/'
+
+def url(endpoint, **kwargs):
+    s = BASE+endpoint+SUFFIX
+    for key in kwargs:
+        s += '&'+key+'='+str(kwargs[key])
+    return s
+
+def search(lat,lng):
+    resp = requests.get(url('venues/search', ll="%s,%s" % (lat, lng), query="Sears", limit=50))
+    return resp.json()['response']['venues']
+
+if __name__ == '__main__':
+    from pprint import pprint as pp
+    pp(search(40.7,-74))
