1ec722e81c50c9c4bd52720e3dd7d3b073fc6137
Damien MARIE <damien@dam.io>
2014-06-07 17:09:44 -0400

    Show error message

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ def post():
     payload = {'venueId': venueId, 'text': text }
     response = requests.post('https://api.foursquare.com/v2/tips/add?oauth_token=0Q235XWN4ACGAPLRUTRUGBQFV1HWN4RVXCMHVE0EAETX2DMC&v=20140607', data=payload) 
     print(response.text)
+    if response.status_code != 200:
+        return response.json()['meta']['errorDetail']
     url = response.json()['response']['tip']['canonicalUrl']
     return redirect(url+'/')
 
